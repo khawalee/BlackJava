3cc00b8643fd1d9233246c10ae668b523458711c
khawalee <134731146+khawalee@users.noreply.github.com>
2023-08-18 15:52:58 -0400

    Card.java
    Final version

--- Card.java
+++ Card.java
@@ -1,25 +1,21 @@
-/**
- * SYST 17796 Project Base code.
- * Students can modify and extend to implement their game.
- * Add your name as an author and the date!
- */
 package ca.sheridancollege.project;
 
-/**
- * A class to be used as the base Card class for the project. Must be general enough to be instantiated for any Card
- * game. Students wishing to add to the code should remember to add themselves as a modifier.
- *
- * @author dancye
- */
-public abstract class Card {
-    //default modifier for child classes
+public class Card {
 
-    /**
-     * Students should implement this method for their specific children classes
-     *
-     * @return a String representation of a card. Could be an UNO card, a regular playing card etc.
-     */
+    private int value;
+    private String suit;
+
+    public Card(int value, String suit) {
+        this.value = value;
+        this.suit = suit;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
     @Override
-    public abstract String toString();
-
+    public String toString() {
+        return value + " of " + suit;
+    }
 }
